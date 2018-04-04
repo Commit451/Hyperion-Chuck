8b3431018f3d1008dc1f4260898ae527f328f427
John <jawnnypoo@gmail.com>
2018-04-04 14:58:48 -0500

    :mag:

---
+++ README.md
@@ -0,0 +1,28 @@
+# Hyperion-Chuck
+[Hyperion](https://github.com/willowtreeapps/Hyperion-Android) Plugin for [Chuck](https://github.com/jgilfelt/chuck)
+[![Build Status](https://travis-ci.org/Commit451/Hyperion-Chuck.svg?branch=master)](https://travis-ci.org/Commit451/Hyperion-Chuck) [![](https://jitpack.io/v/Commit451/Hyperion-Chuck.svg)](https://jitpack.io/#Commit451/Hyperion-Chuck)
+
+## Usage
+First, follow the README to set up [Chuck](https://github.com/jgilfelt/chuck). Then, follow the README to set up [Hyperion](https://github.com/willowtreeapps/Hyperion-Android). Then, all you need to do is simply add the dependency:
+
+```
+allprojects {
+	repositories {
+		...
+		maven { url "https://jitpack.io" }
+	}
+}
+```
+and
+```
+dependencies {
+    debugImplementation 'com.github.Commit451:Hyperion-Chuck:latest.version.here'
+}
+```
+Hyperion will then find the plugin and add it to the list when you open the Hyperion drawer.
+
+## Icon
+Generated from [AndroidAssetStudio](http://romannurik.github.io/AndroidAssetStudio/icons-generic.html), with color set to #8e8e93, with asset padding set to 0dp and asset size set to 30dp.
+
+## License
+Hyperion-Chuck is available under the MIT license. See the LICENSE file for more info.
---
+++ app/src/main/java/com/commit451/hyperion/chuck/sample/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.commit451.hyperion.chuck.sample
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.readystatesoftware.chuck.ChuckInterceptor
+import com.willowtreeapps.hyperion.core.Hyperion
+import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val client = OkHttpClient.Builder()
+                .addInterceptor(ChuckInterceptor(applicationContext))
+                .build()
+
+        buttonCreateTraffic.setOnClickListener {
+            val request = Request.Builder()
+                    .url("https://jsonplaceholder.typicode.com/posts/1")
+                    .build()
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call?, e: IOException?) {
+
+                }
+
+                override fun onResponse(call: Call?, response: Response?) {
+
+                }
+            })
+        }
+
+        buttonOpenHyperion.setOnClickListener {
+            Hyperion.open()
+        }
+    }
+}
